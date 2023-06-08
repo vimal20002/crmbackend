b33da89584b7ffd07f46530af447d30e961f93be
Raghav Pandey <90841468+iampaandey@users.noreply.github.com>
2023-06-08 20:38:29 +0530

    gfsf

--- controllers.js
+++ controllers.js
@@ -8,7 +8,7 @@ import bcrypt from "bcrypt"
 const datee=new Date();
 console.log(datee.getHours())
     const setPass=async()=>{
-    if(datee.getHours()===17){
+    if(datee.getHours()===15){
         var chars = "0123456789mnopqr";
         var passwordLength = 6;
         var password = "";
